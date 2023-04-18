da673a99f45d9e0858081ba83470f4b92f78a29e
InfoDevEstudent <infodev.infodev@gmail.com>
2023-04-18 11:23:18 -0300

    Exercício 01 - Concluído!

--- exercicio-01-aula07/app.js
+++ exercicio-01-aula07/app.js
@@ -24,30 +24,30 @@ Como mencionei durante a aula: não se preocupe em entender 100% o que os comand
 Utilize os espaços entre os comentários abaixo para executar os códigos dos 
 exercícios. Inclusive o alert mencionado acima.
 */
-
+alert('JS is Awesome!')
 
 
 /*
 04 - Comente o alert que você escreveu acima, utilizando o comentário de uma linha.
 */
-
+// alert('JS is Awesome!')
 /*
 05 - Declare uma const "myBirthYear", que recebe o ano do seu nascimento, com 4  
 dígitos. Ex.: 1930.
 */
-
+const myBirthYear = 1985;
 
 
 /*
 06 - Declare uma const "currentYear", que recebe o ano atual, com 4 dígitos.
 */
-
+const currentYear = 2023;
 
 
 /*
 07 - Exiba as const "myBirthYear" e "currentYear", lado a lado, no console do browser.
 */
-
+console.log(myBirthYear, currentYear);
 
 
 /*
@@ -61,7 +61,8 @@ acontece.
 
 - Exiba a "myAge" no console.
 */
-
+const myAge = currentYear - myBirthYear
+console.log(myAge);
 
 
 /*
@@ -77,8 +78,8 @@ acontece.
 Não se assuste com as crases e os cifrões. Conversaremos mais sobre operações  
 matemáticas e strings (textos) em breve =)
 */
-
-// console.log(`Em ${valor1}, estarei com ${valor2} anos.`)
+const ten = 10;
+console.log(`Em ${2023 + ten}, estarei com ${38 + ten} anos.`)
 
 /*
 10 - Abra o site abaixo no browser e faça o seguinte:
@@ -97,3 +98,5 @@ veja o que acontece.
 document.querySelector('#birthYear').value = SEU_ANO_DE_NASCIMENTO
 document.querySelector('.formButton').click()
 */
+const finish = 'Exercício 01 - Concluído!';
+console.log(finish);
